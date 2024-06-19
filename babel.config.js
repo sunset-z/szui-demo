d4d9f445343cda1d434986597a29773c3320eeda
Emily <18312197836@163.com>
2024-06-19 15:23:25 +0800

    fix: previous

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
-    ["@babel/preset-react", { "runtime": "automatic" }],
+    ['@babel/preset-react', { runtime: 'automatic' }],
     '@babel/preset-typescript',
   ],
   plugins: [
@@ -12,6 +12,6 @@ module.exports = {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
       },
     ],
-    './babel-plugin-replace-ext.js'
+    './babel-plugin-replace-ext.js',
   ],
 };
