bda20232ae406a117390166e579bad3aa73a4ece
Emily <18312197836@163.com>
2024-06-24 17:06:45 +0800

    fix: public path

--- craco.config.ts
+++ craco.config.ts
@@ -2,10 +2,17 @@ import { CracoConfig } from '@craco/types';
 import CracoLessPlugin from 'craco-less';
 import path from 'path';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 const config: CracoConfig = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
+    },
+    configure: {
+      output: {
+        publicPath: isDev ? '/' : './',
+      },
     },
   },
   plugins: [
